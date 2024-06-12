149f7dd6824e079d1c4b194056d82786db7c8d39
Ammaar-Alam <ammaar@princeton.edu>
2024-06-12 16:01:16 -0400

    still working on files

--- LectureInfo.java
+++ LectureInfo.java
@@ -1,10 +1,16 @@
 public class LectureInfo {
+
     private String date;
     private String dayOfWeek;
     private int possiblePoints;
     private int earnedPoints;
 
-    public LectureInfo(String date, String dayOfWeek, int possiblePoints, int earnedPoints) {
+    public LectureInfo(
+        String date,
+        String dayOfWeek,
+        int possiblePoints,
+        int earnedPoints
+    ) {
         this.date = date;
         this.dayOfWeek = dayOfWeek;
         this.possiblePoints = possiblePoints;
@@ -19,7 +25,10 @@ public class LectureInfo {
         return earnedPoints;
     }
 
-    public static void main(String[] args) {
-
+    public int updatePoints(int points) {
+        earnedPoints += points;
+        return earnedPoints;
     }
+
+    public static void main(String[] args) {}
 }
