3333a8bd89be9b453b927c7f1574e8ef7240b1de
Ammaar-Alam <ammaar@princeton.edu>
2024-06-14 12:33:54 -0400

    updating pset

--- PsetInfo.java
+++ PsetInfo.java
@@ -1,16 +1,10 @@
 public class PsetInfo {
-    private int psetNumber;          // which pset
     private double psetEarned;          // points earned
     private double psetPossible;        // points possible
 
     public PsetInfo (int psetNumber, double psetEarned, double psetPossible) {
-        this.psetNumber = psetNumber;
         this.psetEarned = psetEarned;
         this.psetPossible = psetPossible;
-    }
-
-    public int getPsetNumber() {
-        return psetNumber;
     }
 
     public double getPsetEarned() {
