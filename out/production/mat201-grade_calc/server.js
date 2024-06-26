1186a9507fbd895d41bcd8a18f381f5cf8fa81fb
Ammaar-Alam <ammaar@princeton.edu>
2024-06-26 16:59:33 -0400

    updating modules

--- out/production/mat201-grade_calc/server.js
+++ out/production/mat201-grade_calc/server.js
@@ -10,25 +10,29 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 app.post("/update-grades", (req, res) => {
-    const { category, dayNumber, earnedPoints } = req.body;
-    console.log("Received request to update grades: ", req.body);
+  const { category, dayNumber, earnedPoints } = req.body;
+  console.log("Received request to update grades: ", req.body);
 
-    const command = `bash server/compile_and_run.sh ${category} ${dayNumber} ${earnedPoints}`;
-    exec(command, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`Error executing script: ${error.message}`);
-            console.error(`stderr: ${stderr}`);
-            res.status(500).json({ success: false, message: `Internal Server Error: ${stderr}` });
-            return;
-        }
-        if (stderr) {
-            console.error(`Script stderr: ${stderr}`);
-            res.status(500).json({ success: false, message: `Internal Server Error: ${stderr}` });
-            return;
-        }
-        console.log(`Script stdout: ${stdout}`);
-        res.json({ success: true, message: "Grades updated successfully" });
-    });
+  const command = `bash server/compile_and_run.sh ${category} ${dayNumber} ${earnedPoints}`;
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing script: ${error.message}`);
+      console.error(`stderr: ${stderr}`);
+      res
+        .status(500)
+        .json({ success: false, message: `Internal Server Error: ${stderr}` });
+      return;
+    }
+    if (stderr) {
+      console.error(`Script stderr: ${stderr}`);
+      res
+        .status(500)
+        .json({ success: false, message: `Internal Server Error: ${stderr}` });
+      return;
+    }
+    console.log(`Script stdout: ${stdout}`);
+    res.json({ success: true, message: "Grades updated successfully" });
+  });
 });
 
 const PORT = process.env.PORT || 3000;
